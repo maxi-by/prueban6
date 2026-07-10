ab8da638ba305f5abb046520f88470b5228e7596
Máximo <maxi.moralesg@duocuc.cl>
2026-07-09 20:12:04 -0400

    Se agregan las funciones mostrar menu y leer opcion

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,1 +1,20 @@
-
+def mostrar_menu():
+    print("""========== MENÚ PRINCIPAL ==========
+1. Consultas totales por especie
+2. Búsqueda de pacientes por rango de costo
+3. Actualizar costo de atención
+4. Registrar nueva mascota
+5. Eliminar registro de mascota
+6. Salir
+====================================""")
+def leer_opcion():
+    while True:
+        try:
+            opc = int(input("Seleccione una opcion: "))
+        except ValueError:
+            print("Error: La opcion debe ser un numero entero")
+        else:
+            if opc >= 1 and opc <= 6:
+                return opc
+            else:
+                print("Error: La opcion ingresada debe estar entre 1-6")
