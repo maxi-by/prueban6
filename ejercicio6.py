3443c2945e032136b0a56cd7965def4f53d55bf5
Máximo <maxi.moralesg@duocuc.cl>
2026-07-09 21:00:11 -0400

    Se agrega el menu para todas las funciones

--- ejercicio6.py
+++ ejercicio6.py
@@ -18,16 +18,6 @@ def leer_opcion():
                 return opc
             else:
                 print("Error: La opcion ingresada debe estar entre 1-6")
-pacientes = {
-    'V001': ['Firulais', 'perro', 'labrador', 5, 'macho', 'Juan Perez'],
-    'V002': ['Michi', 'gato', 'persa', 2, 'hembra', 'Ana Lopez'],
-    'V003': ['Rocky', 'perro', 'bulldog', 8, 'macho', 'Carlos Ruiz'],
-}
-expediente = {
-    'V001': [25000, 2],
-    'V002': [15000, 1],
-    'V003': [30000, 5],
-}
 def total_consultas(especie, pacientes, expediente):
     total = 0
     for clave in pacientes:
@@ -118,3 +108,91 @@ def eliminar_paciente(codigo, expediente, pacientes):
         expediente.pop(codigo)
         return True
     return False
+def main():
+    pacientes = {
+    'V001': ['Firulais', 'perro', 'labrador', 5, 'macho', 'Juan Perez'],
+    'V002': ['Michi', 'gato', 'persa', 2, 'hembra', 'Ana Lopez'],
+    'V003': ['Rocky', 'perro', 'bulldog', 8, 'macho', 'Carlos Ruiz'],
+    }
+    expediente = {
+        'V001': [25000, 2],
+        'V002': [15000, 1],
+        'V003': [30000, 5],
+    }
+    while True:
+        mostrar_menu()
+        opc = leer_opcion()
+        match opc:
+            case 1:
+                especie = input("Ingrese el nombre de la especie: ")
+                total_consultas(especie, pacientes, expediente)
+            case 2:
+                try:
+                    c_min = int(input("Ingrese el costo minimo: "))
+                    c_max = int(input("Ingrese el costo maximo: "))
+                    busqueda_costo(c_min, c_max, pacientes, expediente)
+                except ValueError:
+                    print("Debe ingresar valores enteros")
+            case 3:
+                while True:
+                    codigo = input("Ingrese el codigo de la mascota: ")
+                    try:
+                        costo = int(input("Ingrese el nuevo costo: "))
+                        if actualizar_costo(codigo, costo, expediente):
+                            print("Costo actualizado")
+                        else:
+                            print("El código no existe")
+                    except ValueError:
+                        print("Error: Debe ingresar un numero entero")
+                    continuar = input("¿Desea actualizar otro costo (s/n)?: ")
+                    if continuar != "s":
+                        break
+            case 4:
+                codigo = input("Ingrese el codigo de la mascota: ")
+                if not validar_codigo(codigo) or buscar_codigo(codigo, expediente):
+                    print("Error: Debe ingresar el codigo de la mascota o este mismo ya existe")
+                    continue
+                nombre = input("Ingrese el nombre de la mascota: ")
+                if not validar_nombre(nombre):
+                    print("Error: Debe ingrasar el nombre de la mascota")
+                    continue
+                especie = input("Ingrese la especie de la mascota: ")
+                if not validar_especie(especie):
+                    print("Error: Debe ingresar la especie de la mascota")
+                    continue
+                raza = input("Ingresa la raza de la mascota: ")
+                if not validar_raza(raza):
+                    print("Error: debe ingresar la raza de la mascota")
+                    continue
+                edad = input("Ingrese la edad de la mascota: ")
+                if not validar_edad(edad):
+                    print("Error: Debe ingresar la edad de la mascota")
+                    continue
+                sexo = input("Ingrese el sexo de la mascota: ")
+                if not validar_sexo(sexo):
+                    print("Error: Debe ingresar si su mascota es macho o hembra ")
+                    continue
+                nombre_dueño = input("Ingrese el nombre del dueño: ")
+                if not validar_nombre_dueño(nombre_dueño):
+                    print("Error: Debe ingresar el nombre del dueño")
+                    continue
+                try:
+                    costo = int(input("Ingrese el costo de antencion: "))
+                    consultas = int(input("Ingrese numero de consultas: "))
+                    if validar_costo(costo) and validar_consultas(consultas):
+                        agregar_paciente(codigo, nombre, especie, raza, edad, sexo, nombre_dueño, costo, consultas, expediente, pacientes)
+                        print("Mascota registrada")
+                    else:
+                        print("El código ya existe")
+                except ValueError:
+                    print("Error: El costo debe ser mayor a 0 y la consulta debe ser mayor o igual a 0")
+            case 5:
+                codigo = input("Ingrese el codigo de la mascota: ")
+                if eliminar_paciente(codigo, expediente, pacientes):
+                    print("Registro eliminado")
+                else:
+                    print("El código no existe")
+            case 6:
+                print("Programa finalizado.")
+                break
+main()
